4c37fb4f21bb29bb93ec6ec4f1721e13f921535f
J C <james@jamescampbell.us>
2016-08-11 15:37:15 -0400

    Update give-me-names.sh

--- hide/give-me-names.sh
+++ hide/give-me-names.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -n1 -p "New real name or username? [r,u]" doit 
 case $doit in  
-  r|R|1) telnet koeln.ccc.de;; 
-  u|U|2) telnet koeln.ccc.de 31337 ;; 
-  *) echo "lololololol" ;; 
+  r|R|1) telnet koeln.ccc.de | grep : 
+  u|U|2) telnet koeln.ccc.de 31337 ; 
+  *) echo "lololololol" ;
 esac
