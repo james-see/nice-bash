1420e19d24d43676effb8ac0185b468e793bd66c
J C <james@jamescampbell.us>
2016-09-16 13:41:24 -0400

    added options for special char vs not

--- secure/passme.sh
+++ secure/passme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# What: Creates a password that is 64 characters long including special chars
+# What: Creates either letter&num or letter&num&special char based passwords of length you set 
+# and also can create a list of them as needed
 clear
 echo "";
 echo "";
@@ -10,4 +11,28 @@ echo " )___//(__)\ \__ \\__ \ )    (  )__) ";
 echo "(__) (__)(__)(___/(___/(_/\/\_)(____)";
 echo "";
 echo "[*] the secure password generator using /dev/urandom [*]";
-perl -pe 'tr/A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+=//dc;' < /dev/urandom | head -c 32; echo
+echo ""
+echo ""
+read -n1 -p "with special chars or without? [s,w]: " doit
+echo ""
+default="32"
+read -p "length? recommended at least 32 [32]: " lnn
+lnn="${lnn:-${default}}"
+read -p "how many times? [1]: " hmt
+defaulthmt=1
+hmt="${hmt:-{defaulthmt}}"
+case $doit in
+  s|S|1)
+        for i in `eval echo {1..$hmt}`
+        do
+                perl -pe 'tr/A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+=//dc;' < /dev/urandom | h$
+        done
+        ;;
+w|W|2)
+        for i in `eval echo {1..$hmt}`
+        do
+                perl -pe 'tr/A-Za-z0-9//dc;' < /dev/urandom | head -c $lnn; echo
+        done
+        ;;
+  *) echo "lololololol" ;;
+esac
