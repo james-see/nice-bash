f4421e3a1a31929ae9877b009cbd91e13cd3994e
James Campbell <james@jamescampbell.us>
2018-04-28 21:08:51 -0400

    updated installer to rustup for rust

--- installers/install-rust-hello-world.sh
+++ installers/install-rust-hello-world.sh
@@ -1,9 +1,10 @@
 # this code creates hello_world.rs and turns it into an executable an then runs it
-# optionally it installs rust if you comment out the second line 'brew install rust'
+# optionally it installs rust if you comment out the second line 'curl https://sh.rustup.rs -sSf | sh`
+'
 
 #!/bin/bash
-# optional step install homebrew first and install rust via brew
-# brew install rust
+# optional step install rust via rustup
+# curl https://sh.rustup.rs -sSf | sh
 # found and taken from here: https://joelmccracken.github.io/entries/a-simple-web-app-in-rust-pt-1/
 echo 'fn main() {
 
