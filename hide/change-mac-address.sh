f6554255346055fcf0e4abf4780d3d6e29889bb8
James Campbell <james@jamescampbell.us>
2018-05-08 16:06:07 -0400

    aded grep to show current mac address spoof if there is one

--- hide/change-mac-address.sh
+++ hide/change-mac-address.sh
@@ -4,12 +4,14 @@
 # What: Use pip install spoofmac first then this will allow you to spoof and confirm spoofed mac address
 # Requirements: spoofmac python package, OSX, run as sudo
 # Alternative requirements: install node npm and then npm install spoof -g
-
-echo "Current devices and mac addresses..."
-spoof list
-echo "Randomizing wifi by running sudo spoof randomize en0..."
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+printf "\n${GREEN}Current devices and mac addresses...${NC}\n\n"
+spoof list | grep -o "currently set to.*"
+printf "\nRandomizing wifi by running ${CYAN}sudo spoof randomize en0${NC}...\n"
 sudo spoof randomize en0
-echo "Hard resetting wifi device to ensure spoof is in place..."
+printf "\nHard resetting wifi device to ensure spoof is in place...\n"
 networksetup -setairportpower airport off; networksetup -setairportpower airport on
 spoof list
 echo "Note the Wi-fi device should should a new field for the MAC."
