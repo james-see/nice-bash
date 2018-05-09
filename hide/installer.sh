5d4570c9c2c4650dec806c9adc9fc39f266a7d1e
James Campbell <james@jamescampbell.us>
2018-05-09 17:36:06 -0400

    added installer file to setup pre-requisites for laz-e-script-kids

---
+++ hide/installer.sh
@@ -0,0 +1,8 @@
+# installs all pre-requisites for OSX for this folder
+brew install coreutils # for gshuf for osx-gen-words
+brew install telnet # telnet is removed from high sierra and above muthafuckers
+brew install node
+npm install spoof -g
+
+
+
