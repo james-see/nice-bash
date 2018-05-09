19bdb672e3b1c0ff474e3c9dec5fbdd719f881c0
James Campbell <james@jamescampbell.us>
2018-05-09 17:42:24 -0400

    renamed installer to reflect that it is intended for Apple use.

--- hide/osx-installer.sh
+++ hide/osx-installer.sh
@@ -1,7 +1,8 @@
 # installs all pre-requisites for OSX for this folder
+# for other OS's this is garunteed to not work at all
 brew install coreutils # for gshuf for osx-gen-words
 brew install telnet # telnet is removed from high sierra and above muthafuckers
-brew install node
+brew install node # required for the mac spoofing that works from an npm package
 npm install spoof -g
 
 
