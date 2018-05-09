d56a84f16a36f27d64afbf61d92774cde8333212
James Campbell <james@jamescampbell.us>
2018-05-09 17:10:10 -0400

    updates to get osx-gen-words working

--- hide/osx-gen-words.sh
+++ hide/osx-gen-words.sh
@@ -4,4 +4,5 @@ clear
 read -p "how many names do you need? [1]: " total
 totaldef=1
 total="${total:-${totaldef}}"
+command -v gshuf >/dev/null 2>&1 || { echo >&2 "I require gshuf but it's not installed. On OSX, install via brew install coreutils."; exit 1; }
 gshuf -n${total} /usr/share/dict/words
