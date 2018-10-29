f192433d7777a8aff52cbc8100be8127fc1d0c18
James Campbell <jcampbell@parrylabs.com>
2018-10-29 08:52:36 -0400

    updates

--- osx-tools/find-my-pi.sh
+++ osx-tools/find-my-pi.sh
@@ -1,4 +1,5 @@
 # Find your raspberry pi devices on the network
 # arp -na | grep -i b8:27:eb # does not seem to work anymore
-sudo nmap -sP 192.168.1.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+# the below works but is slow, patience is a virtue
+sudo nmap -sP 10.2.2.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
 
