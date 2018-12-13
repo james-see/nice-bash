8137659bc243fdd10e32d9efd489f86e7d300068
James Campbell <james@jamescampbell.us>
2018-12-13 16:23:37 -0500

    fixed the osx ip address lookup

--- osx-tools/get-current-inet-ip.sh
+++ osx-tools/get-current-inet-ip.sh
@@ -1,2 +1,4 @@
 # change the first 10\. to whatever your format is 192, 172, etc.
-ifconfig | grep -m1 -E "\b(10\.)([0-9]{1,2}\.){2}[0-9]{1,3}\b" -o | head -n1
+# ifconfig | grep -m1 -E "\b(10\.)([0-9]{1,2}\.){2}[0-9]{1,3}\b" -o | head -n1
+# better version
+ifconfig | grep inet | grep "inet " | tail -n1 # targets just inet ip4
