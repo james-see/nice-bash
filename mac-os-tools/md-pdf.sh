05b79f0e0b7fcf731792883933706b9b6a6aa400
James Campbell <jcampbell@anduril.com>
2021-10-29 23:15:46 -0400

    updates

--- mac-os-tools/md-pdf.sh
+++ mac-os-tools/md-pdf.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 # make pre donuts
+
 if which pandoc > /dev/null; then
     echo "pandoc found..."
     if which wkhtmltopdf > /dev/null; then
         echo "wkhtmltopdf found... converting now to output.pdf"
         markdowndoc=$1
         echo $markdowndoc
-        pandoc -f markdown -t $markdowndoc -o output.pdf
+        pandoc $markdowndoc -t pdf -o output.pdf --verbose --pdf-engine=/Library/TeX/texbin/pdflatex
         echo "enjoy"
         exit 0
     else
         brew install pandoc
         brew install wkhtmltopdf --cask
+        brew install librsvg python homebrew/cask/basictex
     fi
 else
     brew install pandoc
     brew install wkhtmltopdf --cask
+    brew install librsvg python homebrew/cask/basictex
 fi
 echo "pandoc and wkhtmltopdf now installed... converting now to output.pdf"
 markdowndoc=$1
 echo $markdowndoc
-pandoc -f markdown -t $markdowndoc -o output.pdf
+pandoc $markdowndoc -t pdf -o output.pdf --verbose --pdf-engine=/Library/TeX/texbin/pdflatex
 echo "enjoy"
 exit 0
 
