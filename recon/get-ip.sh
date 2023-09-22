774139845789a4998d55a0300335d7de2cd84d86
JC <james@jamescampbell.us>
2023-09-22 00:08:07 -0400

    updated get ip

--- recon/get-ip.sh
+++ recon/get-ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This should work on OSX and Debian out of the box after hgrep is installed (sudo npm install -g hgrep)
-curl --silent https://ipof.me | hgrep -a href a | cut -d'/' -f6
+# This should spit out the public ip to ip.txt
+curl --silent https://ifconfig.me > ip.txt
